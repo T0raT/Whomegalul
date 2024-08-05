7eb86d4da3b483a392947fa80f9459eca0d75e1a
T0ra <tigerliu@bu.edu>
2024-08-04 22:41:42 -0400

    Added some other components like Lamp and StarsBackground just to mess around with them. Also added GradientBg to use in BentoGrid

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { FaHome } from "react-icons/fa";
+import { StarsBackground } from "@/components/ui/StarsBackground";
 
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black sm:px-10">
+      <StarsBackground starDensity={0.00025} />
       <div className="w-full max-w-7xl">
         <FloatingNav
           navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -23,27 +23,60 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
   id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
-  id: number;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div
       className={cn(
-        "group/bento relative row-span-1 flex flex-col justify-between space-y-4 rounded-3xl border border-red-500 border-transparent bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento relative row-span-1 flex flex-col justify-between space-y-4 rounded-3xl border border-transparent bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
         className,
       )}
+      style={{
+        background: "rgb(0,0,0)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 27%, rgba(0,75,67,1) 59%, rgba(0,193,184,1) 100%)",
+      }}
     >
-      {header}
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
+        <div className="absolute h-full w-full">
+          <img
+            src={img}
+            alt={img}
+            className={cn(imgClassName, "object-cover, object-center")}
+          />
+        </div>
+        <div
+          className={`absolute -bottom-5 right-0 ${id === 5 && "w-full opacity-80"}`}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className="h-full w-full object-cover object-center"
+            />
+          )}
+        </div>
+        {id === 6 && (
+
+        )}
+      </div>
+
       <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {icon}
         <div className="mb-2 mt-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
         </div>
